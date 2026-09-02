aae390fbb66a0528cfa0c5b8cd86f584b52c0f60
OleJac18 <olejacobsen181997@gmail.com>
2026-09-02 12:30:13 +0200

    Mehrwertsteuer_2 kleine Verbesserungen

--- Mehrwertsteuer_2/Mehrwertsteuer_2.cpp
+++ Mehrwertsteuer_2/Mehrwertsteuer_2.cpp
@@ -9,19 +9,24 @@ int main()
 	setlocale(LC_ALL, "de_DE.utf8");
 	cout << setprecision(2) << fixed;
 
+	//V)ariable
 	double nettobetrag = 0, mwst_satz = 0, mwst;
 
-
+	//E)ingabe
 	cout << "Gebe den Nettobetrag ein:";
 	cin >> nettobetrag;
-
 	cout << "Gebe den Mehrwertsteuersatz ein (in Prozent):";
 	cin >> mwst_satz;
 
+	//Nach der Eingabe, Bildschrim loeschen
+	system("cls");
+
+	//V)erarbeitung
 	mwst = nettobetrag * mwst_satz / 100;
 
 	system("cls");
 
+	//A)usgabe
 	cout << left << setw(30) << setfill('*') << "Nettobetrag:"
 		 << right << setw(10) << setfill('-') << nettobetrag << " Euro" << endl;
 	
